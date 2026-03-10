5250f101a852bee0234280ec7c2a09d5211a6087
Priyesh Mishra <mishrapriyesh898gmail.com>
2026-03-10 14:38:34 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-import express from "express";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app = express();
-app.use(express.json());
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-export default app;
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export { app }
